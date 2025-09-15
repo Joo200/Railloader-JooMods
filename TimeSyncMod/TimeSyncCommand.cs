7037acdab934966bc3d7134cd9b2cf60e3ba8d87
JOO200 <github@joo200.de>
2025-09-15 23:01:14 +0200

    Initial commit

---
+++ TimeSyncMod/TimeSyncCommand.cs
@@ -0,0 +1,18 @@
+﻿using Game;
+using Game.Messages;
+using Game.State;
+using UI.Console;
+
+namespace TimeSyncMod
+{
+    [ConsoleCommand("/timesync", "Sync the time to all clients")]
+    public class TimeSyncCommand : IConsoleCommand
+    {
+        public string Execute(string[] components)
+        {
+            GameDateTime timeCursor = TimeWeather.Now;
+            StateManager.ApplyLocal(new SetTimeOfDay((float)timeCursor.TotalSeconds));
+            return "synced time";
+        }
+    }
+}
---
+++ TimeSyncMod/TimeSyncMod.cs
@@ -0,0 +1,19 @@
+﻿using Network.Client;
+using Railloader;
+using Serilog;
+using UI.Builder;
+
+namespace TimeSyncMod
+{
+    public class TimeSyncMod : PluginBase
+    {
+        ILogger logger = Log.ForContext<TimeSyncMod>();
+
+        public TimeSyncMod(IModdingContext moddingContext, IModDefinition self)
+        {
+            logger.Information("Registering time sync command!");
+
+            moddingContext.RegisterConsoleCommand(new TimeSyncCommand());
+        }
+    }
+}
